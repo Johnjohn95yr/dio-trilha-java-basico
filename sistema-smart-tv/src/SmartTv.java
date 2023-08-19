f97a2a342209dae967e1ba3b0092b8f2cd673c84
joaoluizGsilva <johnjohnr27@gmail.com>
2023-08-19 17:02:47 -0300

    Subindo outros aquivos

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,17 @@
+public class SmartTv {
+      boolean ligada = false; 
+      int canal = 1;
+      int volume = 10;
+
+      public void ligar(){
+        ligada = true;
+      }
+      public void desligar(){
+        ligada = false;
+      }
+      
+      public void aumentarVolume(){
+        volume = 32;
+      }
+    
+}
---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+          //operador ternário
+          int a = 5;
+          int b = 6;
+        //atribuição(condição) (se sim)       (se não)
+          String result = a!=b ? "Verdadeiro" : "Falso";
+          System.out.println(result);
+
+    }
+}
